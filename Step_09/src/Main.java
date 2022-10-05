b8a3facd747912a6b6f3ba9dc431b9157432ee00
Lee-Seongchan <lseong96@gmail.com>
2022-10-06 00:28:52 +0900

    Study
    Study

--- Step_09/src/Main.java
+++ Step_09/src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
 
@@ -195,9 +197,39 @@ public class Main {
                 }
             }
             //======================================정렬 끝.
+            //향상된 for 문
+            //for(자료형 변수명 : 배열명){
+            //	문장
+            //}
             System.out.println("정렬된 값을 출력합니다");
             for(int element : numbers)
                 System.out.println(element);
+
+            // =================[선택 정렬]========================== 정렬 시작.
+
+            // 한 번의 배열 탐색에서 가장 작은 원소의 '위치'를 찾고,
+            // 그 위치와 배열의 가장 첫 번째 원소부터 차례로 바꿔주는 방식을 사용(오름 차순으로 정렬하는 경우)하는 정렬 방식
+
+            // ===================================================== 정렬 끝. 내림차순으로
+
+            for(int i = 0; i < numbers.length - 1; i++) {
+                // 현재 탐색에서 가장 앞의 원소를 초기 값으로 설정해둔다.
+                int MinIndex = i;
+                // 탐색을 진행하며, 가장 작은 값을 찾는다.
+                for(int j = i + 1; j < numbers.length; j++) {
+                    if(numbers[MinIndex] > numbers[j])
+                        MinIndex = j;
+                }
+                // 탐색이 완료되면 가장 작은 값을 가장 앞의 원소와 가장 작은 원소의 위치를 바꾸어준다.
+                int temp = numbers[MinIndex];
+                numbers[MinIndex] = numbers[i];
+                numbers[i] = temp;
+
+                System.out.println("<정렬 후>");
+                for(int element : numbers)
+                    System.out.println(element);
+            }
+
         }
 
 
