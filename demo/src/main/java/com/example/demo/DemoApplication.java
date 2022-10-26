f8bdf8b4ba260583daf2bf1ecc543c319812e69a
Lee-Seongchan <lseong96@gmail.com>
2022-10-26 13:28:22 +0900

    TEST2
    TEST2

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -3,12 +3,10 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-// 스프링부트가 @에서부터 시작(출발지점) - main
 @SpringBootApplication
 public class DemoApplication {
 
-	public static void main(String[] args)
-	{
+	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
